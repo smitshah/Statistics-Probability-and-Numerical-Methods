ded57e2072eba849c2f5105cdd26c65070702818
Smit Shah <smitshah0885@gmail.com>
2020-04-29 16:08:25 +0530

    Add files via upload

---
+++ Regression lines/regression_lines.R
@@ -0,0 +1,11 @@
+print("Exp No.: 9")
+print("Name: Smit Shah      PRN: 17070123090       Batch: EB-2")
+x<-c(1,2,3,4,5,6,7)
+x
+y<-c(4,5,6,7,8,9,10)
+y
+cor(x,y)
+var(x)
+var(y)
+conv(x,y)
+plot(x,y)
