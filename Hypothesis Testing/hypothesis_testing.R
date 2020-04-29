98fcb1aba97baa90374174e30c9eddf2044d8dd7
Smit Shah <smitshah0885@gmail.com>
2020-04-29 16:26:54 +0530

    Add files via upload

---
+++ Hypothesis Testing/hypothesis_testing.R
@@ -0,0 +1,18 @@
+print("Exp No.: 10")
+print("Name: Smit Shah      PRN: 17070123090       Batch: EB-2")
+d=seq(1,20, by=1)
+d
+m=mean(d)
+m
+s=sd(d)
+s
+smit<-t.test(d,alternative="two.sided", mu=6, conf.int=0.95)
+smit
+d1=seq(1,85, by=1)
+d1
+m1=mean(d1)
+m1
+s1=sd(d1)
+mu=8
+valz=(m1-mu)/(s1/sqrt(length(d1)))
+valz
