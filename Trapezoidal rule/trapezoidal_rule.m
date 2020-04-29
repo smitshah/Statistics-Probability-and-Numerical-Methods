df60e2f190b4c7e5e481b77368fdea6c24720fb3
Smit Shah <smitshah0885@gmail.com>
2020-04-29 15:53:51 +0530

    Add files via upload

---
+++ Trapezoidal rule/trapezoidal_rule.m
@@ -0,0 +1,21 @@
+% Evaluate using trapezoidal rule
+clc
+clear all
+close all
+%diary('trapezoidal_rule.txt')
+syms x;
+a=input('Enter the value of lower limit of the intergration a: ');
+b=input('Enter the value of upper limit of the intergration b: ');
+n=input('enter the number of subinterval n: ');
+f=input('Enter f: ');
+f=inline(f);
+h=(b-a)/n;
+sum=0;
+for i=1:n-1
+    x=a+i*h;
+    sum=sum+f(x);
+    result=(h/2)*(f(a)+f(b)+2*sum);
+end
+fprintf('the result of this integral is: ');
+disp(result);
+%dairy('off')
